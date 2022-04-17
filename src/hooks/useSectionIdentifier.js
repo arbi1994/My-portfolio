9f75f9506d5072386c59f4cc937eeb377e32031e
arbi1994 <arbi.1994@hotmail.com>
2022-04-17 17:48:54 +1000

    Increased scrollTracker intersection observer's root margin as the buttons where not getting the background color assigned

--- src/hooks/useSectionIdentifier.js
+++ src/hooks/useSectionIdentifier.js
@@ -10,7 +10,7 @@ const useSectionIdentifier = (el) => {
   const [rootMargin, setRootMargin] = useState(height)
 
   useEffect(() => {
-    setRootMargin(height/2 - 0.5)
+    setRootMargin(height/2 - 0.6)
   }, [height])
 
   const { inView, ref } = useInView({
